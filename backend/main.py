186623a1d57d2b2c174627d02187633a68772357
ud39 <ud-gaming@gmx.de>
2023-06-11 16:15:30 +0200

    Rewrite relations class, add solution and language relation as well as the endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,23 @@
+import struct
 from pydantic import BaseModel
 from fastapi import FastAPI
-from sql_app.database import store_input_data
+from sql_app.database import store_input_data, store_language, store_solution
 
 
 class InputDataRequest(BaseModel):
     data: str
+    year: int
+    day: int
+
+
+class LanguageDataRequest(BaseModel):
+    language: str
+    logo: bytes
+
+
+class SolutionDataRequest(BaseModel):
+    language: str
+    solution: str
     year: int
     day: int
 
@@ -18,5 +31,16 @@ async def root():
 
 
 @app.post("/input")
-async def input(input: InputDataRequest):
+async def inputData(input: InputDataRequest):
     return store_input_data(input.data, input.year, input.day)
+
+
+@app.post("/language")
+async def inputLanguage(input: LanguageDataRequest):
+    binary_data = struct.pack('>Q', 121312312313123)
+    return store_language(LanguageDataRequest.language, LanguageDataRequest.logo)
+
+
+@app.post("/solution")
+async def inputSolution(input: SolutionDataRequest):
+    return store_solution(SolutionDataRequest.solution, SolutionDataRequest.language, SolutionDataRequest.year, SolutionDataRequest.day)
--- backend/sql_app/database.py
+++ backend/sql_app/database.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine, Integer, String, Column, CheckConstraint
+from sqlalchemy import create_engine, Integer, String, Column, CheckConstraint, ForeignKey, LargeBinary
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, mapped_column, relationship
 
 
 SQLALCHEMY_DATABASE_URL = "postgresql://jutiboottawong:@localhost/advent_of_code"
@@ -14,15 +14,28 @@ Base = declarative_base()
 class InputData(Base):
     __tablename__ = 'input_data'
 
-    def __init__(self, data, year, day):
-
-        self.data = data
-        self.year = year
-        self.day = day
-
     data = Column(String, nullable=False)
-    year = Column(Integer, CheckConstraint('year > 2000'), primary_key=True)
-    day = Column(Integer, CheckConstraint('day > 0 AND day < 32'), primary_key=True)
+    year = Column(Integer, CheckConstraint('year > 2015'), primary_key=True)
+    day = Column(Integer, CheckConstraint('day > 0 AND day < 26'), primary_key=True)
+
+
+class Language(Base):
+    __tablename__ = 'language'
+
+    id = mapped_column(Integer, primary_key=True)
+    solution = relationship("Solution", back_populates="language")
+    language = Column(String, nullable=False)
+    logo = Column(LargeBinary, nullable=False)
+
+
+class Solution(Base):
+    __tablename__ = 'solution'
+
+    id = mapped_column(Integer, primary_key=True)
+    language_id = mapped_column(ForeignKey("language.id"))
+    language = relationship("Language", back_populates="solution")
+    year = Column(Integer, CheckConstraint('year > 2015'), primary_key=True)
+    day = Column(Integer, CheckConstraint('day > 0 AND day < 26'), primary_key=True)
 
 
 # Base.metadata.create_all(engine, checkfirst=True)
@@ -37,3 +50,29 @@ def store_input_data(data: str, year: int, day: int):
     session.add(data)
     session.commit()
     session.close()
+
+
+def store_language(language: str, logo: bytes):
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    data = Language(language=language, logo=logo)
+    session.add(data)
+    session.commit()
+    session.close()
+
+
+def store_solution(solution: str, language: str, year: int, day: int):
+
+    Session = sessionmaker(bind=engine)
+    languageID = getLanguageID(language)
+    session = Session()
+    data = Solution(solution=solution, languageId=languageID, year=year, day=day)
+    session.add(data)
+    session.commit()
+    session.close()
+
+
+def getLanguageID(language: str) -> int:
+    pass
+    # TODO: select language and get id
