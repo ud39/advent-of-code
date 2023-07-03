69e962b028281b64d695f22b63d5c1e674a05a08
ud39 <ud-gaming@gmx.de>
2023-07-03 21:33:07 +0200

    Add basic authority for one single user.

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,13 @@
 from pydantic import BaseModel
-from fastapi import FastAPI, UploadFile, Form
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi import FastAPI, UploadFile, Form, HTTPException, Depends
+
+from dotenv import load_dotenv
 from sql_app.database import store_input_data, store_language, store_solution
+
+import os
+
+load_dotenv()
 
 
 class InputDataRequest(BaseModel):
@@ -17,20 +24,36 @@ class SolutionDataRequest(BaseModel):
 
 
 app = FastAPI()
+security = HTTPBasic()
+
+
+def validate_token(credentials: HTTPBasicCredentials):
+    user = os.getenv("TOKEN")
+    valid_token = os.getenv("SECRET")
+
+    if credentials.username == user and credentials.password == valid_token:
+        return True
+    else:
+        raise HTTPException(
+            status_code=401,
+            detail='Invalid username or password',
+            headers={"WWW-Authenticate": "Basic"},
+        )
 
 
 @app.get("/")
-async def root():
+async def root(credentials: HTTPBasicCredentials = Depends(security)):
+    validate_token(credentials)
     return {"message": "Hello World"}
 
 
 @app.post("/input")
-async def inputData(input: InputDataRequest):
+async def inputData(input: InputDataRequest, credentials: HTTPBasicCredentials = Depends(security)):
     return store_input_data(input.data, input.year, input.day)
 
 
 @app.post("/language")
-async def inputLanguage(lang: str = Form(...), file: UploadFile = UploadFile(...)):
+async def inputLanguage(lang: str = Form(...), file: UploadFile = UploadFile(...), credentials: HTTPBasicCredentials = Depends(security)):
 
     file_data = await file.read()
     file_string = file_data.decode("utf-8")
@@ -39,5 +62,5 @@ async def inputLanguage(lang: str = Form(...), file: UploadFile = UploadFile(...
 
 
 @app.post("/solution")
-async def inputSolution(input: SolutionDataRequest):
+async def inputSolution(input: SolutionDataRequest, credentials: HTTPBasicCredentials = Depends(security)):
     return store_solution(SolutionDataRequest.solution, SolutionDataRequest.language, SolutionDataRequest.year, SolutionDataRequest.day)
