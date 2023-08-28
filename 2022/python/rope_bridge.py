455b550d9f5edcada6dd7643e1e6d865a2705a9b
ud39 <ud-gaming@gmx.de>
2023-08-28 21:54:51 +0200

    Change approach from functional to use classes/objects due to part 2.

--- 2022/python/rope_bridge.py
+++ 2022/python/rope_bridge.py
@@ -2,65 +2,73 @@
 with open('input_09.txt', 'r') as f:
     lines = [line.strip() for line in f]
 
-possible_moves = {
-    'R': 1,
-    'L': -1,
-    'U': 1,
-    'D': -1
-}
+
+def sign(x):
+    return (x > 0) - (x < 0)
+
+class HeadKnot:
+
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def move(self, direction: str):
+        if direction == 'R':
+            self.x = self.x + 1
+            self.y = self.y
+
+        if direction == 'L':
+            self.x = self.x - 1
+            self.y = self.y
+
+        if direction == 'U':
+            self.x = self.x
+            self.y = self.y + 1
+
+        if direction == 'D':
+            self.x = self.x
+            self.y = self.y - 1
+
+        return self
 
 
-def adjacent(head, tail):
-    if abs(head[0] - tail[0]) > 1 or abs(head[1] - tail[1]) > 1:
-        return True
-    else:
-        return False
+class Knot(HeadKnot):
 
+    def __init__(self, x, y, visited_place):
+        super().__init__(x, y)
+        self.visited_place = visited_place
+        self.visited_place.add((0, 0))
 
-def rope_bridge_one(path):
+    def follow_head(self, head_knot: HeadKnot):
 
-    head = (0, 0)
-    tail = (0, 0)
+        dx = head_knot.x - self.x
+        dy = head_knot.y - self.y
 
-    number_of_touch_points = 1
-    visited_places = set()
-    visited_places.add((0, 0))
-
-    for move in path:
-
-        move = move.split()
-        direction = move[0]
-        amount_of_move = int(move[1])
-
-        if direction == 'R' or direction == 'L':
-            for i in range(0, amount_of_move):
-                head = (head[0] + 1 * possible_moves[direction], head[1])
-
-                if abs(head[0] - tail[0]) > 1 and abs(head[1] - tail[1]) == 1:
-                    tail = (tail[0] + 1 * possible_moves[direction], head[1])
-                    visited_places.add(tail)
-                    number_of_touch_points = number_of_touch_points + 1
-                    continue
-                if adjacent(head, tail):
-                    tail = (tail[0] + 1 * possible_moves[direction], tail[1])
-                    visited_places.add(tail)
-                    number_of_touch_points = number_of_touch_points + 1
+        if abs(dx) > 1 or abs(dy) > 1:
+            new_x = self.x + sign(dx)
+            new_y = self.y + sign(dy)
+            self.visited_place.add((new_x, new_y))
+            return Knot(new_x, new_y, self.visited_place)
         else:
-            for i in range(0, amount_of_move):
-                head = (head[0], head[1] + 1 * possible_moves[direction])
+            return self
 
-                if abs(head[1] - tail[1]) > 1 and abs(head[0] - tail[0]) == 1:
-                    tail = (head[0], tail[1] + 1 * possible_moves[direction])
-                    visited_places.add(tail)
-                    number_of_touch_points = number_of_touch_points + 1
-                    continue
-                if adjacent(head, tail):
-                    tail = (tail[0], tail[1] + 1 * possible_moves[direction])
-                    visited_places.add(tail)
-                    number_of_touch_points = number_of_touch_points + 1
+    def amount_of_visited_place(self):
 
-    print(visited_places)
-    return len(visited_places)
+        return len(self.visited_place)
 
 
-rope_bridge_one(lines)
+head = HeadKnot(0, 0)
+knot = Knot(0, 0, set())
+
+
+for move in lines:
+
+    move = move.split()
+    direction = move[0]
+    amount_of_move = int(move[1])
+
+    for i in range(0, amount_of_move):
+        knot = knot.follow_head(head.move(direction))
+
+
+print(knot.amount_of_visited_place())
