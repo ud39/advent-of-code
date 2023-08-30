33c128916faf0a18936b9146575280c35b1b74a4
ud39 <ud-gaming@gmx.de>
2023-08-30 15:51:04 +0200

    Part 2 of day 10 simple solution.

--- 2022/python/cathode-ray-tube.py
+++ 2022/python/cathode-ray-tube.py
@@ -3,6 +3,7 @@ with open('input_10.txt', 'r') as f:
     signals = [line.rstrip() for line in f]
 
 
+# Part One
 def signal_strength(signals):
 
     initial_value = 1
@@ -36,4 +37,44 @@ def signal_strength(signals):
     return signal_strengths
 
 
-print(sum(signal_strength(signals)))
+# Part Two
+def decode_signal(signals):
+
+    initial_value = 1
+    current_cycle = 0
+    i = 0
+    rows = 6
+    amount_of_cycles = 40
+    signal_grid = [['' for _ in range(amount_of_cycles)] for _ in range(rows)]
+
+    for sig in signals:
+
+        if 'noop' in sig:
+            if abs(current_cycle - initial_value) <= 1:
+                signal_grid[i][current_cycle] = '#'
+            else:
+                signal_grid[i][current_cycle] = '.'
+
+            current_cycle = (current_cycle + 1) % 40
+            if current_cycle == 0:
+                i = (i + 1) % rows
+        else:
+            for _ in range(0, 2):
+                if abs(current_cycle - initial_value) <= 1:
+                    signal_grid[i][current_cycle] = '#'
+                else:
+                    signal_grid[i][current_cycle] = '.'
+
+                current_cycle = (current_cycle + 1) % 40
+                if current_cycle == 0:
+                    i = (i + 1) % rows
+
+            initial_value = initial_value + int(sig.split(' ')[1])
+
+    return signal_grid
+
+
+message = decode_signal(signals)
+
+for row in message:
+    print(row)
