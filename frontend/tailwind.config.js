49678c25d4ed45e8a8bb1b0b3dcd4516b1a04d74
ud39 <ud-gaming@gmx.de>
2023-11-09 22:01:10 +0100

    Add placeholder shimmer animation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        'shine-animation': "shine 3s linear infinite"
+        'shine-animation': "shine 3s linear infinite",
+        'placeholder-shimmer-animation': "shimmer 2s infinite normal forwards"
       },
       keyframes: {
         shine: {
@@ -19,6 +20,14 @@ module.exports = {
             'background-size': '200% 200%',
             'background-position': 'right center',
           },
+        },
+        shimmer: {
+          '0%': {
+            'transform': 'translateX(-100%)'
+          },
+          '100%': {
+            'transform': 'translate(100%)'
+          }
         }
       }
     },
