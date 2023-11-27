57c58b736fc1d9ce3a6a8ca82edaa2b2e73b5756
ud39 <ud-gaming@gmx.de>
2023-11-27 21:46:18 +0100

    Add dark theme toggle to storyboard and toggle is via data-mode attribute.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,36 +1,34 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
+  darkMode: ["class", '[data-mode="dark"]'],
   theme: {
     extend: {
       animation: {
-        'shine-animation': "shine 3s linear infinite",
-        'placeholder-shimmer-animation': "shimmer 2s infinite normal forwards"
+        "shine-animation": "shine 3s linear infinite",
+        "placeholder-shimmer-animation": "shimmer 2s infinite normal forwards",
       },
       keyframes: {
         shine: {
-          '0%,100%': {
-            'background-size': '200% 200%',
-            'background-position': 'left center',
+          "0%,100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
           },
-          '50%': {
-            'background-size': '200% 200%',
-            'background-position': 'right center',
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
           },
         },
         shimmer: {
-          '0%': {
-            'transform': 'translateX(-100%)'
+          "0%": {
+            transform: "translateX(-100%)",
           },
-          '100%': {
-            'transform': 'translate(100%)'
-          }
-        }
-      }
+          "100%": {
+            transform: "translate(100%)",
+          },
+        },
+      },
     },
   },
   plugins: [],
-}
+};
