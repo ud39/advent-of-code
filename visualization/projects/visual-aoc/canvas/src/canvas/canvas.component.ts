5d2102bc30d580b42b55774c6a2921d5cd9b02c3
ud39 <ud-gaming@gmx.de>
2024-10-07 21:44:54 +0200

    Add: Skeleton component canvas and expose it as entry point.

---
+++ visualization/projects/visual-aoc/canvas/src/canvas/canvas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-canvas',
+  standalone: true,
+  imports: [],
+  templateUrl: './canvas.component.html',
+  styleUrl: './canvas.component.css'
+})
+export class CanvasComponent {
+
+}
