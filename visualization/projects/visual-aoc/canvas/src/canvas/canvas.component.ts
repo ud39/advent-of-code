220c776a7f584937f7acb243c67873cd178d35fe
ud39 <ud-gaming@gmx.de>
2024-10-08 22:51:14 +0200

    Add: Simple canvas with border

--- visualization/projects/visual-aoc/canvas/src/canvas/canvas.component.ts
+++ visualization/projects/visual-aoc/canvas/src/canvas/canvas.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'lib-canvas',
   standalone: true,
   imports: [],
   templateUrl: './canvas.component.html',
-  styleUrl: './canvas.component.css'
+  styleUrl: './canvas.component.css',
 })
-export class CanvasComponent {
+export default class CanvasComponent implements AfterViewInit {
+  canvasWidth: number = 200;
+  canvasHeigth: number = 200;
+  @ViewChild('mainCanvas') mainCanvasRef!: ElementRef<HTMLCanvasElement>;
+  constructor() {}
 
+  ngAfterViewInit(): void {
+    this.setupCanvas();
+  }
+
+  setupCanvas() {
+    const canvasCtx = this.mainCanvasRef.nativeElement.getContext('2d');
+    if (!canvasCtx) return;
+    this.setupBorder(canvasCtx);
+  }
+
+  setupBorder(canvasCtx: CanvasRenderingContext2D) {
+    canvasCtx.strokeStyle = 'black';
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeRect(0, 0, this.canvasWidth, this.canvasHeigth);
+  }
 }
